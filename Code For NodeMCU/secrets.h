b409be1bb74283dc5e764a3995fe07cf9da2b943
CuriosityWeekend <curiosityweekends+git@gmail.com>
2025-05-07 16:28:41 +0530

    Update secrets.h

--- Code For NodeMCU/secrets.h
+++ Code For NodeMCU/secrets.h
@@ -4,5 +4,7 @@
 const char* ssid = "";
 const char* password = "";
 const char* mqtt_server = "192.168.x.x";  // Adjust accordingly
+const char* mqtt_username = "mqtt_user";
+const char* mqtt_password = "yourpassword";
 
 #endif
